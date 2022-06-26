08bc14728242d206a6323ba10b5272e9caf93761
maniyarmohammedsaqlain <85744349+maniyarmohammedsaqlain@users.noreply.github.com>
2022-06-26 14:04:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ document.addEventListener("keypress",function(ev){
 function makeSound(key){
   switch (key) {
     case "w":
-        var tom1= new Audio("sounds/tom-1.mp3");
+        var tom1= new Audio("tom-1.mp3");
         tom1.play();
       break;
     case "a":
-        var tom2= new Audio("sounds/tom-2.mp3");
+        var tom2= new Audio("tom-2.mp3");
         tom2.play();
       break;
     case "s":
-        var tom2= new Audio("sounds/tom-3.mp3");
+        var tom2= new Audio("tom-3.mp3");
         tom2.play();
       break;
     case "d":
-        var tom2= new Audio("sounds/tom-4.mp3");
+        var tom2= new Audio("tom-4.mp3");
         tom2.play();
       break;
     case "j":
-        var tom2= new Audio("sounds/crash.mp3");
+        var tom2= new Audio("crash.mp3");
         tom2.play();
       break;
     case "k":
-        var tom2= new Audio("sounds/kick-bass.mp3");
+        var tom2= new Audio("kick-bass.mp3");
         tom2.play();
       break;
     case "l":
-        var tom2= new Audio("sounds/snare.mp3");
+        var tom2= new Audio("snare.mp3");
         tom2.play();
       break;
     default:
